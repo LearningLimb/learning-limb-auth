749fec0c8ac33d5f9c6d76daf6d4b4905e4f7b1f
Limbo <admin@learninglimb.com>
2016-10-18 13:16:28 -0400

    Removed bash style eval in favor of a node module to facilitate running on Windows

--- bin/prestart.js
+++ bin/prestart.js
@@ -1,9 +1,25 @@
 #! /usr/bin/env node
 'use strict';
-
+const env = require('node-env-file');
 const root = require('@ddlees/module-root');
 const shell = require('shelljs');
 const DB_PATH = `${root.path}/tmp/db`;
+
+try {
+    env(`${root.path}/development.env`);
+} catch (e) {
+    if (process.env.NODE_ENV !== 'production'){
+        console.log(`
+    ///////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////
+    //                                                                               //
+    //  WARNING: Starting server in development mode without a development.env file  //
+    //                                                                               //   
+    ///////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////
+        `);
+    }
+}
 
 if (process.env.NODE_ENV !== 'production') {
     if (!shell.which('mongod')) {
--- bin/start.js
+++ bin/start.js
@@ -5,10 +5,8 @@ const root = require('@ddlees/module-root');
 const shell = require('shelljs');
 
 const WWW = `${root.path}/bin/www.js`;
-const ENV = `${root.path}/development.env`;
-const EVAL = `eval $([ -f ${ENV} ] && cat ${ENV})`;
 const NODE = process.env.NODE_ENV === 'production' ?
   'node' :
   `${root.path}/node_modules/.bin/nodemon --config ${root.path}/nodemon.json`;
 
-shell.exec(`${EVAL} ${NODE} ${WWW}`, {async:true});
+shell.exec(`${NODE} ${WWW}`, {async:true});
