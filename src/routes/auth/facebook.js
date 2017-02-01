5a1111414290ebfffff51dc5a58b77d61d49a789
Limbo <admin@learninglimb.com>
2017-01-31 19:48:58 -0500

    Added REDIRECT_HOST envvar

--- src/routes/auth/facebook.js
+++ src/routes/auth/facebook.js
@@ -6,8 +6,8 @@ const auth = require('../../lib/auth');
 router.get('/', auth.authenticate('facebook', {scope: 'public_profile'}));
 
 router.get('/callback', auth.authenticate('facebook', {
-    successRedirect: '/home',
-    failureRedirect: '/login'
+    successRedirect: `${process.env.REDIRECT_HOST}/home`,
+    failureRedirect: `${process.env.REDIRECT_HOST}/login`
 }));
 
 module.exports = router;
--- src/routes/auth/google.js
+++ src/routes/auth/google.js
@@ -6,8 +6,8 @@ const auth = require('../../lib/auth');
 router.get('/', auth.authenticate('google', {scope: 'profile'}));
 
 router.get('/callback', auth.authenticate('google', {
-    successRedirect: '/home',
-    failureRedirect: '/login'
+    successRedirect: `${process.env.REDIRECT_HOST}/home`,
+    failureRedirect: `${process.env.REDIRECT_HOST}/login`
 }));
 
 module.exports = router;
