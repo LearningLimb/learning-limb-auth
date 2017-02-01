8d9d16b8918daa282a2c3ea1f9e7bc0db48df88b
Limbo <admin@learninglimb.com>
2017-01-31 20:20:10 -0500

    Fixed Redirect host in strategies

--- src/lib/auth/facebook.js
+++ src/lib/auth/facebook.js
@@ -4,7 +4,7 @@ const verifyProvider = require('./verifyProvider');
 module.exports = new FacebookStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: '/auth/facebook/callback',
+  callbackURL: `${process.env.REDIRECT_HOST}/auth/facebook/callback`,
   passReqToCallback: true,
   profileFields: ['id', 'displayName', 'first_name', 'last_name', 'gender', 'photos', 'email']
 }, verifyProvider('facebook'));
--- src/lib/auth/google.js
+++ src/lib/auth/google.js
@@ -4,7 +4,7 @@ const verifyProvider = require('./verifyProvider');
 module.exports = new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback',
+  callbackURL: `${process.env.REDIRECT_HOST}/auth/google/callback`,
   passReqToCallback: true,
   profileFields: ['id', 'displayName', 'first_name', 'last_name', 'gender', 'photos', 'email']
 }, verifyProvider('google'));
